5cdbc2e01d34e62f8e179f3b4a1521693fa94278
Jasse Nousiainen <jasse.nousiainen@aalto.fi>
2019-02-18 16:32:08 +0200

    declutter top menu + version number

--- topmain.php
+++ topmain.php
@@ -12,12 +12,8 @@ echo "<header>";
   if (isset($_SESSION['logged_in_user'])) {
     echo "<a href='/index.php' style='margin-left:20px;'><i class='fas fa-clock'></i> Etusivu</a>";
     echo "<a href='/mypage.php'><i class='fas fa-user'></i> Oma sivu</a>";
-    echo "<a href='/reports/quickreport.php'><i class='fas fa-user'></i> Omat tunnit</a>";
     if ($_SESSION['logged_in_user']->admin == 1) {
       echo "<a href='/admin/index.php'><i class='fas fa-toolbox'></i> Hallinta</a>";
-    }
-    if ($_SESSION['logged_in_user']->reports == 1) {
-      echo "<a href='/reports/index.php'><i class='fas fa-calendar-alt'></i> Raportit</a>";
     }
     echo '</nav>';
 
