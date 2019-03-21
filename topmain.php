20bfeffd6ce0045f448ef03945e1917f3a83729d
Jasse Nousiainen <jasse.nousiainen@aalto.fi>
2019-03-21 17:27:26 +0200

    Kirjaus- ja omasivunappien ulkoasua parannettu

--- topmain.php
+++ topmain.php
@@ -10,15 +10,24 @@ echo "<header>";
   </a>';
 
   if (isset($_SESSION['logged_in_user'])) {
-    echo "<a href='/index.php' style='margin-left:20px;'><i class='fas fa-clock'></i> Etusivu</a>";
-    echo "<a href='/mypage.php'><i class='fas fa-user'></i> Oma sivu</a>";
-    /*if ($_SESSION['logged_in_user']->admin == 1) {
+    if ($_SERVER['REQUEST_URI'] != '/timeclock.php') {
+      echo "<a href='/index.php' style='margin-left:20px;'><i class='fas fa-clock'></i> Etusivu</a>";
+    }
+    if ($_SESSION['logged_in_user']->admin == 1) {
       echo "<a href='/admin/index.php'><i class='fas fa-toolbox'></i> Hallinta</a>";
-    }*/
+    }
+    if ($_SESSION['logged_in_user']->time_admin == 1) {
+      echo "<a href='/time_editor.php'><i class='fas fa-toolbox'></i> Kellotuseditori</a>";
+    }
+    if ($_SESSION['logged_in_user']->reports == 1) {
+      echo "<a href='/reports/total_hours.php'><i class='fas fa-toolbox'></i> Raportit</a>";
+    }
     echo '</nav>';
 
     echo " <div class='loggedBar'>";
-    echo $_SESSION['logged_in_user']->displayname.": <a href='/logout.php'>Kirjaudu Ulos <i class='fas fa-sign-out-alt'></i></a>";
+    echo $_SESSION['logged_in_user']->displayname .": ";
+    echo "<a href='/mypage.php'><i class='fas fa-user'></i> Oma sivu</a>";
+    echo "<a href='/logout.php'><i class='fas fa-sign-out-alt'></i> Kirjaudu Ulos</a>";
   } else {
     echo '</nav>';
     echo " <div class='loggedBar'>";
