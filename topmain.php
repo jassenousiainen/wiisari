3b169b8842a56588ac62091614675dbf64b291cb
Jasse Nousiainen <jasse.nousiainen@aalto.fi>
2019-03-21 14:03:04 +0200

    Ulkoasun hiensäätöä, sekä todo lista

--- topmain.php
+++ topmain.php
@@ -12,9 +12,9 @@ echo "<header>";
   if (isset($_SESSION['logged_in_user'])) {
     echo "<a href='/index.php' style='margin-left:20px;'><i class='fas fa-clock'></i> Etusivu</a>";
     echo "<a href='/mypage.php'><i class='fas fa-user'></i> Oma sivu</a>";
-    if ($_SESSION['logged_in_user']->admin == 1) {
+    /*if ($_SESSION['logged_in_user']->admin == 1) {
       echo "<a href='/admin/index.php'><i class='fas fa-toolbox'></i> Hallinta</a>";
-    }
+    }*/
     echo '</nav>';
 
     echo " <div class='loggedBar'>";
@@ -22,7 +22,7 @@ echo "<header>";
   } else {
     echo '</nav>';
     echo " <div class='loggedBar'>";
-    echo '<a href="/loginpage.php"><i class="fas fa-sign-in-alt"></i> Kirjaudu</a>';
+    echo '<a href="/loginpage.php"><i class="fas fa-sign-in-alt"></i> Oma sivu</a>';
   }
 echo "</div>";
 
