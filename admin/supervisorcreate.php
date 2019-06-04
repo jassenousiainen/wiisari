2908df5c26d679b2ca2df601d1c44595a6d2f0b7
Jasse Nousiainen <jasse.nousiainen@aalto.fi>
2019-06-04 13:09:27 +0300

    korjauksia valvojan luontiin

--- admin/supervisorcreate.php
+++ admin/supervisorcreate.php
@@ -6,6 +6,8 @@ include "$_SERVER[DOCUMENT_ROOT]/topmain.php";
 
 echo "<title>Kellotuseditori</title>\n";
 
+include 'header_get.php';
+
 $self = $_SERVER['PHP_SELF'];
 $request = $_SERVER['REQUEST_METHOD'];
 
@@ -26,23 +28,23 @@ if  ( isset($_POST['create']) ) {
         if (!empty($empfullnamecheck)) {$error = true; $empfullname = "error";}
     }
 
-    if (!isset($_POST['displayname'])) {$error = true; $displayname = "error";}
+    if (!isset($_POST['displayname']) || $_POST['displayname'] == "") {$error = true; $displayname = "error";}
     else {$displayname = $_POST['displayname'];}
 
-    if (!isset($_POST['password'])) {$error = true; $password = "error";}
+    if (!isset($_POST['password']) || $_POST['password'] == "") {$error = true; $password = "error";}
     else {$password = $_POST['password'];}
 
-    if (!isset($_POST['barcode'])) {$error = true; $barcode = "error";}
+    if (!isset($_POST['barcode']) || $_POST['barcode'] == "") {$error = true; $barcode = "error";}
     else {
         $barcode = $_POST['barcode'];
         $barcodecheck = mysqli_fetch_row(tc_query( "SELECT barcode FROM employees WHERE barcode = '$barcode'"));
         if (!empty($barcodecheck)) {$error = true; $barcode = "error";}
     }
 
-    if (!isset($_POST['office_name'])) {$error = true; $office = "error";}
+    if (!isset($_POST['office_name']) || $_POST['office_name'] == "") {$error = true; $office = "error";}
     else {$office = $_POST['office_name'];}
 
-    if (!isset($_POST['group_name'])) {$error = true; $group = "error";}
+    if (!isset($_POST['group_name']) || $_POST['group_name'] == "") {$error = true; $group = "error";}
     else {$group = $_POST['group_name'];}
 
 
@@ -51,14 +53,14 @@ if  ( isset($_POST['create']) ) {
         echo '<section class="container">
             <div class="middleContent">
             <div class="box">
-                <h2>Luo uusi valvoja (luonnissa tapahtui virhe!)'.$group.'</h2>
+                <h2>Luo uusi valvoja (luonnissa tapahtui virhe!)'.$_POST['group_name'].'</h2>
                 <div class="section">
                 <form name="form" action="'.$self.'" method="post">
                 <table>
                 <tbody>';
         if ($empfullname == "error") {
             echo '  <tr>
-                        <p class="errortext">Käyttäjätunnus oli varattu tai tyhjä, täytäthän sen uudelleen:</p>
+                        <td class="errortext" colspan="3">Käyttäjätunnus oli varattu tai tyhjä, täytäthän sen uudelleen:</td>
                     </tr>
                     <tr>
                         <td>Käyttäjätunnus:</td>
@@ -70,7 +72,7 @@ if  ( isset($_POST['create']) ) {
         }
         if ($displayname == "error") {
             echo '  <tr>
-                        <p class="errortext">Nimi oli tyhjä, täytäthän sen uudelleen:</p>
+                        <td class="errortext" colspan="3">Nimi oli tyhjä, täytäthän sen uudelleen:</td>
                     </tr>
                     <tr>
                         <td>Nimi:</td>
@@ -82,7 +84,7 @@ if  ( isset($_POST['create']) ) {
         }
         if ($password == "error") {
             echo '  <tr>
-                        <p class="errortext">Salasana oli tyhjä, täytäthän sen uudelleen:</p>
+                        <td class="errortext" colspan="3">Salasana oli tyhjä, täytäthän sen uudelleen:</td>
                     </tr>
                     <tr>
                         <td>Salasana:</td>
@@ -94,7 +96,7 @@ if  ( isset($_POST['create']) ) {
         }
         if ($barcode == "error") {
             echo '  <tr>
-                        <p class="errortext">Viivakoodi oli varattu tai tyhjä, täytäthän sen uudelleen:</p>
+                        <td class="errortext" colspan="3">Viivakoodi oli varattu tai tyhjä, täytäthän sen uudelleen:</td>
                     </tr>
                     <tr>
                         <td>Viivakoodi:</td>
@@ -104,28 +106,23 @@ if  ( isset($_POST['create']) ) {
         } else {
             echo '<input name="barcode" value="'.$barcode.'" type="hidden">';
         }
-        if ($office == "error") {
+        if ($office == "error" || $group == "error") {
             echo '  <tr>
-                        <p class="errortext">Et valinnut toimistoa, valitsethan sen uudelleen:</p>
+                        <td class="errortext" colspan="3">Et valinnut toimistoa tai ryhmää, valitsethan uudelleen:</td>
                     </tr>
                     <tr>
                         <td>Toimisto:</td>
                         <td><select name="office_name" onchange="group_names();" required="true"></select></td>
                         <td style="color: grey; font-size: 12px;">Toimisto, johon valvoja kuuluu</td>
                     </tr>';
-        } else {
-            echo '<input name="office_name" value="'.$office.'" type="hidden">';
-        }
-        if ($group == "error") {
+        
             echo '  <tr>
-                        <p class="errortext">Et valinnut ryhmää, valitsethan sen uudelleen:</p>
-                    </tr>
-                    <tr>
                         <td>Ryhmä:</td>
                         <td><select name="group_name" required="true"></select></td>
                         <td style="color: grey; font-size: 12px;">Ryhmä, johon valvoja kuuluu</td>
                     </tr>';
-        }  else {
+        } else {
+            echo '<input name="office_name" value="'.$office.'" type="hidden">';
             echo '<input name="group_name" value="'.$group.'" type="hidden">';
         }
         echo '
