86f265f8f15acd8b4bb57116594c59db9bb46b53
Jasse Nousiainen <jasse.nousiainen@aalto.fi>
2019-07-03 13:16:49 +0300

    header siivottu

--- header.php
+++ header.php
@@ -13,47 +13,64 @@ echo "<head>\n";
 echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>';
 
 
+/* ----- JQuery ----- */
 echo '<script type="text/javascript" src="/scripts/jquery-3.1.1.min.js"></script>';
 echo '<script type="text/javascript" src="/scripts/jquery-ui.min.js"></script>';
-echo '<link rel="stylesheet" href="/scripts/jquery-ui.min.css">';
-echo '<script src="/scripts/datepicker-fi.js"></script>';
+if ($_SERVER['REQUEST_URI'] != '/timeclock.php') {
+  echo '<link rel="stylesheet" href="/scripts/jquery-ui.min.css">';
+  echo '<script src="/scripts/datepicker-fi.js"></script>';
+}
+/* ------------------ */
 
-// different css for front page and the rest of pages
+
+/* ----- CSS ----- */
 if ($_SERVER['REQUEST_URI'] == '/timeclock.php') {
   echo '<link rel="stylesheet" type="text/css" media="screen" href="/css/gradient.css" id="theme"/>';
-  echo '<script type="text/javascript" src="/scripts/wiisari.js"></script>';
 } else {
   echo '<link rel="stylesheet" type="text/css" media="screen" href="/css/default.css" id="theme" />'; 
 }
 if ($_SERVER['REQUEST_URI'] == '/employees/employeeinfo.php' || $_SERVER['REQUEST_URI'] == '/barcode-generator/barcodefetch.php') {
   echo '<link rel="stylesheet" type="text/css" href="/css/barcode-generator.css"/>';
 }
+/* --------------- */
 
-if (isset($_SESSION['logged_in_user']) && $_SERVER['REQUEST_URI'] == '/mypage.php') {
+
+/* ----- Custom JS scripts ----- */
+if ($_SERVER['REQUEST_URI'] == '/timeclock.php') {
+  echo '<script type="text/javascript" src="/scripts/wiisari.js"></script>';
+} 
+else if ($_SERVER['REQUEST_URI'] == '/mypage.php') {
   echo '<script type="text/javascript" src="/scripts/mypage.js"></script>';
+} 
+else if ($_SERVER['REQUEST_URI'] == '/employees/employeecreate.php') {
+  echo '<script type="text/javascript" src="/scripts/employeecreate.js"></script>';
+} 
+else if ($_SERVER['REQUEST_URI'] == '/employees/employeeinfo.php') {
+  echo '<script type="text/javascript" src="/scripts/employeeinfo.js"></script>';
+}
+/* ----------------------------- */
+
+
+/* ----- Plugins ----- */
+if ($_SERVER['REQUEST_URI'] == '/mypage.php') {
+  /* chartJS */
   echo '<script type="text/javascript" src="/scripts/Chart.bundle.min-v2.7.3.js"></script>';
   echo '<script type="text/javascript" src="/scripts/chartjs-plugin-deferred.min.js"></script>';
 }
-if ($_SERVER['REQUEST_URI'] == '/employees/employeecreate.php') {
-  echo '<script type="text/javascript" src="/scripts/employeecreate.js"></script>';
+if ($_SERVER['REQUEST_URI'] != '/timeclock.php') {
+  /* Tablesorter */
+  echo '<link rel="stylesheet" href="/css/wiisari.tablesorter.css">';
+  echo '<script type="text/javascript" src="/scripts/tablesorter/jquery.tablesorter.js"></script>';
+  echo '<script type="text/javascript" src="/scripts/tablesorter/jquery.tablesorter.widgets.js"></script>';
+  echo '<link rel="stylesheet" href="/scripts/tablesorter/jquery.tablesorter.pager.css">';
+  echo '<script type="text/javascript" src="/scripts/tablesorter/jquery.tablesorter.pager.js"></script>';
 }
-if ($_SERVER['REQUEST_URI'] == '/employees/employeeinfo.php') {
-  echo '<script type="text/javascript" src="/scripts/employeeinfo.js"></script>';
-}
-/* wiisari tablesorter theme */
-echo '<link rel="stylesheet" href="/css/wiisari.tablesorter.css">';
-/* tablesorter plugin */
-echo '<script type="text/javascript" src="/scripts/tablesorter/jquery.tablesorter.js"></script>';
-/* tablesorter widget file */
-echo '<script type="text/javascript" src="/scripts/tablesorter/jquery.tablesorter.widgets.js"></script>';
-/* pager plugin */
-echo '<link rel="stylesheet" href="/scripts/tablesorter/jquery.tablesorter.pager.css">';
-echo '<script type="text/javascript" src="/scripts/tablesorter/jquery.tablesorter.pager.js"></script>';
+
+echo '<link rel="stylesheet" href="/fonts/fontawesome/css/all.min.css">';
+/* ------------------- */
 
 
 echo '<link rel="shortcut icon" href="/images/wiisari_title.png" type="image/x-icon"/>';
-echo '<link rel="stylesheet" href="/fonts/fontawesome/css/all.min.css">';
-
 
 ?>
 <body>
